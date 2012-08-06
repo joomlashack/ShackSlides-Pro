b6cf3820ee84eb9cd4833114e9bef1e2a61495a6
Víctor López <v@playtodeath.com>
2012-08-05 20:50:10 -0600

    code tested and fixed.

--- helpers/visionary.php
+++ helpers/visionary.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /**
@@ -9,7 +10,11 @@
 
 defined('_JEXEC') or die('Direct access to files is not permitted');
 
+@define('JPATH_ADMIN_JSVISIONARY', JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jsvisionary');
+@define('JPATH_SITE_JSVISIONARY', JPATH_SITE . DS . 'components' . DS . 'com_jsvisionary');
+
 require_once(JPATH_ROOT.DS.'modules'.DS.'mod_shackslides'.DS.'helper.php');
+jimport( 'joomla.application.component.helper' );
 
 class ModShackSlidesVisionaryHelper extends ModShackSlidesHelper {
 
@@ -54,7 +59,7 @@ class ModShackSlidesVisionaryHelper extends ModShackSlidesHelper {
 				case ' ordering ':
 					$query .= ' ordering ';
 					break;
-				case 'created'
+				case 'created':
 					$query .= ' creation_date ';
 					break;
 			}
@@ -74,13 +79,110 @@ class ModShackSlidesVisionaryHelper extends ModShackSlidesHelper {
 
 		foreach($this->content as $item) {
 
-			$this->images []= $item->image;
+			$this->images []= $this->createImageUrl($item->image);
 			$this->titles []= $item->title;
 			$this->links []= $item->url;
 
 		}
 
+		$this->base = '';
+
 	}
+
+
+	private function createImageUrl($path) {
+
+
+		// Protect against back dir
+		if (DS == "\\")
+			$ds = "\\\\";
+		else
+			$ds = "\/";
+
+		$path = preg_replace("/\.\." .$ds. "/", "", $path);
+
+		$markers = $this->getMarkers();
+
+		if (!preg_match("/\[.+\]/", $path))
+			$path = '[DIR_JSSSSLIDE_IMAGE]' . $path;
+
+		//DIR SPECIFIC FOLDERS (Starts with DIR... )
+		foreach($markers as $marker => $pathStr)
+			if (substr($marker, 0, 3) == "DIR")
+				$path = preg_replace("/\[" . $marker . "\]/", $pathStr, $path);
+
+		//OTHER MARKERS
+		foreach($markers as $marker => $pathStr)
+			if (substr($marker, 0, 3) != "DIR")
+				$path = preg_replace("/\[" . $marker . "\]/", $pathStr, $path);
+
+
+		$path = preg_replace("/\[.+\]/", "", $path);  // Clean tags if remains
+		
+		//convert absolute server path to URL
+		$url = JURI::root() . str_replace(JPATH_BASE . '/', '', $path);
+
+		return $url;
+
+
+	}
+
+
+
+	private function getMarkers() {
+
+		$configMedias = JComponentHelper::getParams('com_media');
+		$config = JComponentHelper::getParams('com_jsvisionary');
+
+		$markers = array(
+			'DIR_JSSSSLIDE_IMAGE' => $config->get("upload_dir_jsssslide_image", JPATH_SITE . DS . $config->get('image_path', 'images') . DS . "jsssslide_image") .DS,
+			'DIR__TRASH' => $config->get("trash_dir", JPATH_ADMIN_JSVISIONARY .DS. "images" . DS . "trash") .DS,
+
+			'COM_ADMIN' => JPATH_ADMIN_JSVISIONARY,
+			'ADMIN' => JPATH_ADMINISTRATOR,
+			'COM_SITE' => JPATH_SITE_JSVISIONARY,
+			'IMAGES' => JPATH_SITE .DS. $config->get('image_path', 'images')  .DS,
+			'MEDIAS' => JPATH_SITE .DS. $configMedias->get('file_path', 'images') .DS,
+			'ROOT' => JPATH_SITE
+		);
+
+
+
+		return $markers;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
