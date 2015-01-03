abdccc4fd5cb95a9eb306ceafd38d1159c7ad88c
Tito Alvarez <tito@joomlashack.com>
2015-01-03 13:10:41 -0600

    MODSHSLIDE-76 #resolve #comment Added informational width parameter

---
+++ code/site/fields/information.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package     Shackslides
+ * @subpackage  Fields
+ *
+ * @copyright   Copyright (C) 2010 - 2015 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Restrict Access to within Joomla
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+
+JFormHelper::loadFieldClass('spacer');
+
+/**
+ * Field just for loading javascript helper to hide fields on other fields dependencies
+ *
+ * @package     Wright
+ * @subpackage  Parameters
+ * @since       3.0
+ */
+class JFormFieldInformation extends JFormFieldSpacer
+{
+	public $type = 'Information';
+
+	/**
+	 * Method to get the field input markup for a spacer.
+	 * The spacer does not have accept input.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		return '<p>' . JText::_($this->description) . '</p>';
+	}
+}
